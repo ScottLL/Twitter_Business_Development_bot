8f55a5555f3eec50e87f71aa11e778662691034a
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2022-11-07 16:02:24 -0500

    Update searching_tweets.py

--- searching_tweets.py
+++ searching_tweets.py
@@ -90,7 +90,7 @@ def searchTweets(query, max_results):
 
 if __name__ == '__main__':
 #       coins = ['BTC','ETH','DOGE','ADA','BNB','XRP','SOL','MATIC','DOT','STETH','SHIB','TRX','DAI','UNI','WBTC','LTC','LEO','OKB','ATOM','LINK','FTT','XLM','CRO','XMR','ALGO','NEAR','TON']
-      coins = ['DAI','UNI','WBTC','LTC','LEO'] 
+    coins = ['DAI','UNI','WBTC','LTC','LEO'] 
     for i in coins:
         searchTweets(i, int(200/len(coins)))
         time.sleep(60*15)
