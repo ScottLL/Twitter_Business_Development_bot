d2da443fea9f824d32a79aaf262696f0530edcb8
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2022-11-20 18:50:01 -0500

    Update main_new_coin.py

--- main_new_coin.py
+++ main_new_coin.py
@@ -5,8 +5,8 @@ from searching_tweets import searchTweets
 #     coins = ['BTC','ETH','DOGE','ADA','BNB','XRP','SOL','MATIC','DOT','STETH','SHIB','TRX','DAI','UNI','WBTC','LTC','LEO','OKB','ATOM','LINK','FTT','XLM','CRO','XMR','ALGO','NEAR','TON']
 #     coins = ['BTC','ETH','DOGE','ADA','DAI','UNI','WBTC','LTC','LEO','FTT','ALGO','NEAR','TON'] 
 # coins = ['BTC','ETH']
-# coins = ['XLM','CRO','XMR','OKB','ATOM','LINK','BNB','XRP']
-# for i in coins:
-#     searchTweets(i, int(250/len(coins)))
+coins = ['XLM','CRO','XMR','OKB','ATOM','LINK','BNB','XRP']
+for i in coins:
+    searchTweets(i, int(250/len(coins)))
 
-searchTweets("BTC", 10)
+# searchTweets("BTC", 10)
