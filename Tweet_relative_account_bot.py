fe35445971d9531e7da5f4b5d69a6f2929a84a6b
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2022-11-22 13:44:05 -0500

    Update Tweet_relative_account_bot.py

--- Tweet_relative_account_bot.py
+++ Tweet_relative_account_bot.py
@@ -14,37 +14,46 @@ def twitter_account_bot():
     auth.set_access_token(token, token_secret)
     api = tweepy.API(auth)
 
-    while True:
-        print(f"\n{datetime.datetime.now()}\n")
-        user_name = [
-            "MEXC_Global",
-            "MEXC_Eilla",
-            "MEXC_VIP",
-            "MEXCDerivatives",
-            "EtfMexc",
-            "MEXC_SEA",
-            "MEXC_Fans",
-            "MEXC_CEO",
-        ]
-        for i in range(len(user_name)):
-            var = tweepy.Cursor(
-                api.search_tweets, q=user_name[i], count=20, result_type="popular"
-            ).items(10)
-            for tweet in var:
-                try:
-                    tweet_id = dict(tweet._json)["id"]
-                    tweet_text = dict(tweet._json)["text"]
 
-                    print("id: " + str(tweet_id))
-                    print("text: " + str(tweet_text))
 
-                    api.retweet(tweet_id)
+    # timeout variable can be omitted, if you use specific value in the while condition
+    timeout = 300   # [seconds]
 
-                except tweepy.TweepyException as error:
-                    print(error)
+    timeout_start = time.time()
 
-            delay = 30
-            time.sleep(delay)
+    while time.time() < timeout_start + timeout:
+            while True:
+                print(f"\n{datetime.datetime.now()}\n")
+                user_name = [
+                    "MEXC_Global",
+                    "MEXC_Eilla",
+                    "MEXC_VIP",
+                    "MEXCDerivatives",
+                    "EtfMexc",
+                    "MEXC_SEA",
+                    "MEXC_Fans",
+                    "MEXC_CEO",
+                ]
+                for i in range(len(user_name)):
+                    var = tweepy.Cursor(
+                        api.search_tweets, q=user_name[i], count=20, result_type="popular"
+                    ).items(10)
+                    for tweet in var:
+                        try:
+                            tweet_id = dict(tweet._json)["id"]
+                            tweet_text = dict(tweet._json)["text"]
+
+                            print("id: " + str(tweet_id))
+                            print("text: " + str(tweet_text))
+
+                            api.retweet(tweet_id)
+
+                        except tweepy.TweepyException as error:
+                            print(error)
+
+                    delay = 30
+                    time.sleep(delay)
+            break
             
 if __name__ in "__main__":
     twitter_account_bot()
