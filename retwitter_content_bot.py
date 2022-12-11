29f2ff706a9f8bbb1b4bbd397158071ef867043e
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2022-12-11 22:31:46 +0000

    upload credentials into github secrect

--- retwitter_content_bot.py
+++ retwitter_content_bot.py
@@ -4,16 +4,15 @@ import tweepy
 import urllib.request
 from tweepy.auth import OAuthHandler
 import os
-
+import json
 
 def retwitter_content_bot():
 
     consumer_key = os.environ["CONSUMER_KEY"]
     consumer_secret = os.environ["CONSUMER_SECRET"]
-    Bearer_Token = os.environ["BEARER_TOKEN"]
     token = os.environ["TOKEN"]
     token_secret = os.environ["TOKEN_SECRET"]
-
+    GOOGLE_SERVICE_ACCOUNT_DATA = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT_DATA"])
     t = Twitter(auth=OAuth(token, token_secret, consumer_key, consumer_secret))
 
     auth = OAuthHandler(consumer_key, consumer_secret)
@@ -21,7 +20,7 @@ def retwitter_content_bot():
     api = tweepy.API(auth)
 
     # Open a sheet from a spreadsheet i
-    gc = gspread.service_account("credentials.json")
+    gc = gspread.service_account_from_dict(GOOGLE_SERVICE_ACCOUNT_DATA)
     wks = gc.open("retweet_bot").sheet1
 
     # Update a range of cells using the top left corner address
