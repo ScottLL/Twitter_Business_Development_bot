bb55e9ac5f0cc2a3aa933b8e0c1ccf2b696e9146
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2022-12-14 22:13:27 -0500

    Update direct_messages.py
    Signed-off-by: Scott Lai <43226003+ScottLL@users.noreply.github.com>

--- directMessage/direct_messages.py
+++ directMessage/direct_messages.py
@@ -23,7 +23,7 @@ def direct_messages(message):
     sort_df = sort_df.reset_index()
     # print(sort_df.tail(10))
     user_id = []
-    for i in range(len(sort_df[-500:-200])):  #len(sort_df)):[300:550]
+    for i in range(len(sort_df[-1000:-1])):  #len(sort_df)):[300:550]
         try:
             user = int(api.get_user(screen_name=sort_df.username[i]).id_str) # get the user id
             last_dms = user in user_id_dataFrame['user_id'].unique() # check if the user id is already in the user_id.csv
